12206fa4890d2972b51b603b1ad87a8d6f5e802c
Aliakbar2 <aliakbarmohsin3@gmail.com>
2020-04-21 12:19:48 +0530

    First commit to master

--- blog_project/mysite/blog/forms.py
+++ blog_project/mysite/blog/forms.py
@@ -9,9 +9,7 @@ class PostForm(forms.ModelForm):
         widgets = {
             'title':forms.TextInput(attrs={'class':'textinputclass'}),
             'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea postcontent'}),
-
         }
-
 
 
 class CommentForm(forms.ModelForm):
@@ -19,7 +17,7 @@ class CommentForm(forms.ModelForm):
     class Meta():
         model = Comment
         fields = ('author', 'text')
-        widgets{
+    widgets = {
             'author':forms.TextInput(attrs={'class':'textinputclass'}),
             'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea'}),
         }
